4b1fd281a50b46e065f2bc6d18c35ddbc1ac4014
adday <adday@email.arizona.edu>
2019-10-21 14:11:31 -0400

    Added test

--- BankersAlgorithm.py
+++ BankersAlgorithm.py
@@ -1,15 +1,18 @@
 from typing import List
 from typing import Tuple
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 class BankersAlgorithm:
-    def __init__(self, num_proc: int, num_rec: int, resources: List[int],
+    def __init__(self, num_proc: int, num_res: int, resources: List[int],
                  allocation: List[List[int]], maximum: List[List[int]]):
         """Initialize the Bankers Algorithm
 
         Args:
             num_proc (int): Number of processes
-            num_rec (int): Number of resources
+            num_res (int): Number of resources
             resources (List[int]): Quantity of resources available
             allocation (List[List[int]]): Current resource allocation
                                           for processes
@@ -18,35 +21,115 @@ class BankersAlgorithm:
 
         # Update all of the datamembers
         self.num_proc = num_proc
-        self.num_rec = num_rec
+        self.num_res = num_res
         self.resources = resources
         self.allocation = allocation
         self.maximum = maximum
 
-        # Check the safety of the current configuration
-        self.safety()
+    def calculate_available(self) -> List[int]:
+        """Calculate the available array. This contains the number of
+        free resources that are not allocated to a process. available[i] = k
+        means that there are k instances of resource i free
 
-    def calculate_available(self):
-        available = [0] * self.num_rec
-        for i in range(len(available)):
+        Returns:
+            List[int]: a List, num_res long, that contains the avaiable
+                       resource count
+        """
+
+        # Seed the list with all 0s
+        available = [0] * self.num_res
+
+        # Loop over all of the resources
+        for i in range(self.num_res):
+            # Set the available_i equal to the number of unallocated resources
             available[i] = (self.resources[i] -
                             sum([allocation[i] for allocation
                                  in self.allocation]))
+
+        # Return the available array
         return available
 
-    def calculate_need(self):
+    def calculate_need(self) -> List[List[int]]:
+        """Calculate the need array. This contains the number of resources
+        that a process could request. need[i][j] = k means that process i
+        could request k more instances of resource j
+
+        Returns:
+            List[List[int]]: The need array
+        """
+        # Return Mat(maximum) - Mat(allocation)
         return [[self.maximum[i][j] - self.allocation[i][j]
-                 for j in range(self.num_rec)] for i in range(self.num_proc)]
+                 for j in range(self.num_res)] for i in range(self.num_proc)]
 
     def request(self, proc_num: int, resource_req: List[int]) -> bool:
+        """Request for a process to be allocated extra resources
+
+        Args:
+            proc_num (int): Process number that the request is being made on
+            resource_req (List[int]): Number of extra resources requested
+
+        Returns:
+            bool: If the request was carried out and the system is currently
+                  in a safe state
+
+        Raises:
+            ValueError: If either the process number is out of range
+        """
+        # Check that the process number is not out of range
+        if proc_num >= self.num_proc or proc_num < 0:
+            raise ValueError("Requested process number: " +
+                             f"{proc_num} is invalid")
+
+        # Check that the resource request is of the correct size
+        if len(resource_req) != self.num_res:
+            raise ValueError("Length of resource request " +
+                             f"{len(resource_req)} is not {self.num_res}")
+
+        # Calculate the need and available arrays
+        need = self.calculate_need()
+        available = self.calculate_available()
+
+        # Set the flag for valid request to true
+        valid_request = True
+
+        # Check if any of the values of the request either go above
+        # the maximum bound for the process or exceed the number
+        # of available resources for the system
+        for i in range(self.num_res):
+            if (resource_req[i] > need[proc_num][i] or
+                    resource_req[i] > available[i]):
+                # If the request is invalid then log and set the flag
+                valid_request = False
+                break
+
+        # If the request is valid then add the resources to the allocation
+        # for that process. No need to update the need or available arrays
+        # because they are recalculated during at each function where they
+        # may be modified
+        if valid_request:
+            for i in range(self.num_res):
+                self.allocation[proc_num][i] += resource_req[i]
+
+        # Check if any of the allocations are below zero and that
+        # the system is currently in a safe state
+        if min(self.allocation[proc_num]) < 0 or not self.safety()[0]:
+            # If the system is not safe then unset the valid flag and reset the
+            # requested resources
+            valid_request = False
+            for i in range(self.num_res):
+                self.allocation[proc_num][i] -= resource_req[i]
+
+        # Return if the request was valid and the system is in a current safe
+        # state
+        return valid_request
 
     def safety(self) -> Tuple[bool, List[int]]:
         """Check the safety of the current state of the system.
-        
+
         Returns:
             Tuple[bool, List[int]]: 2-tuple containing if the system is safe,
                                     and if so the safe process order
-        """                    
+        """
         # Start all processes out as unfinished
         finish = [False] * self.num_proc
 
@@ -73,24 +156,25 @@ class BankersAlgorithm:
                     continue
 
                 can_do = True
-                for j in range(self.num_rec):
-                    if need[i][j] > work[j]:
+                for j in range(self.num_res):
+                    if need[i][j] <= work[j]:
+                        continue
+                    else:
                         can_do = False
+                        break
 
                 if can_do:
-                    print("Finishing: {}".format(i))
                     found_i = True
                     process_order.append(i)
                     finish[i] = True
-                    for j in range(self.num_rec):
-                        work[j] += need[i][j]
+                    for j in range(self.num_res):
+                        work[j] += self.allocation[i][j]
                     break
 
-            if not found_i:
+            if not found_i or min(finish):
                 done = True
 
         return min(finish), process_order
-
 
 
 # 5         (Number of processes)
@@ -106,16 +190,4 @@ class BankersAlgorithm:
 # 9 0 2   (Max for P2)
 # 2 2 2   (Max for P3)
 # 4 3 3   (Max for P4)
-BankersAlgorithm(5, 3, [10, 5, 7],
 
-                 [[0, 1, 0],
-                  [2, 0, 0],
-                  [3, 0, 2],
-                  [2, 1, 1],
-                  [0, 0, 2]],
-
-                 [[7, 5, 3],
-                  [3, 2, 2],
-                  [9, 0, 2],
-                  [2, 2, 2],
-                  [4, 3, 3]])
---
+++ BankersAlgorithmTest.py
@@ -0,0 +1,129 @@
+import unittest
+from BankersAlgorithm import BankersAlgorithm as ba
+
+
+class BankersAlgorithmTestCases(unittest.TestCase):
+    def setUp(self):
+        """Set up the Bankers Algorithm object with the data from the
+        text file given to us by Dr. Kim
+        """
+        self.b = ba(5, 3, [10, 5, 7],
+                    [[0, 1, 0],
+                     [2, 0, 0],
+                     [3, 0, 2],
+                     [2, 1, 1],
+                     [0, 0, 2]],
+                    [[7, 5, 3],
+                     [3, 2, 2],
+                     [9, 0, 2],
+                     [2, 2, 2],
+                     [4, 3, 3]])
+
+    def test_need_calculation_is_valid(self):
+        """Test that the calculation for the need array is producing the
+        correct response
+        """
+        # Define the correct array response
+        correct_need_array = [[7, 4, 3],
+                              [1, 2, 2],
+                              [6, 0, 0],
+                              [0, 1, 1],
+                              [4, 3, 1]]
+
+        # Assert that they are equal
+        self.assertEqual(self.b.calculate_need(), correct_need_array)
+
+    def test_available_calculation_is_valid(self):
+        """Test that the calculation for the available array is producing
+        correct response
+        """
+        # Define the correct array response
+        correct_available_array = [3, 3, 2]
+
+        # Assert that they are equal
+        self.assertEqual(self.b.calculate_available(), correct_available_array)
+
+    def test_invalid_process_number_in_request(self):
+        """Test that giving an invalid (read: out of range) process number
+        will raise a ValueError
+        """
+        with self.assertRaises(ValueError):
+            # Out of range above
+            self.b.request(3, [1, 0, 0])
+            # Out of range below
+            self.b.request(-1, [1, 0, 0])
+
+    def test_invalid_resource_length_in_request(self):
+        """Test that giving an invalid (read: wrong length) array will raise
+        a ValueError
+        """
+        with self.assertRaises(ValueError):
+            # Too long
+            self.b.request(0, [0, 1, 1, 1])
+            # Too short
+            self.b.request(0, [0, 1])
+
+    def test_request_exceeds_process_maximum_allocation(self):
+        """Test that giving a process request that exceeds the given
+        maximal process resources will return false and not request
+        those resources
+        """
+        # Assert that an invalid request returns false because
+        # it goes above the maximum allocation
+        self.assertFalse(self.b.request(0, [8, 0, 0]))
+        # Assert that the allocation for process 0 didn't go through
+        self.assertEqual(self.b.allocation[0], [0, 1, 0])
+
+    def test_request_deceeds_zero(self):
+        """Test that a request that brings the current allocation
+        for a process below zero throws a ValueError
+        """
+        # Assert that a request that would put the resource allocation
+        # below zero returns false
+        self.assertFalse(self.b.request(0, [-1, 0, 0]))
+        # Assert that the invalid allocation didn't go through
+        self.assertEqual(self.b.allocation[0], [0, 1, 0])
+
+    def test_request_puts_system_in_unsafe_state(self):
+        """Test that a request that puts the system into an unsafe
+        state will not be carried out
+        """
+        self.assertFalse(self.b.request(4, [3, 3, 0]))
+        self.assertEqual(self.b.allocation[4], [0, 0, 2])
+
+    def test_valid_request(self):
+        """Test that giving a valid request changes the allocation,
+        available, and need arrays correctly
+        """
+        # Assert that the correct request returns true
+        self.assertTrue(self.b.request(0, [0, 2, 0]))
+        # Assert that the allocation has been reflected in the object
+        self.assertEqual(self.b.allocation[0], [0, 3, 0])
+
+    def test_safe_safety_sequence(self):
+        """Test that a safe sequence does exist for the current Bankers
+        Algorithm configuration and that it is in fact a valid sequence
+        """
+        # Query the safety of the configuration
+        is_safe, safe_seq = self.b.safety()
+
+        # Assert that the configuration is tested as safe
+        self.assertTrue(is_safe)
+
+        # Check if the sequence can actually run
+        for process in safe_seq:
+            # For each allocation for process p assert that each of
+            # the allocations are less than the current available
+            # system resources
+            for i in range(self.b.num_res):
+                self.assertLess(self.b.allocation[process][i],
+                                self.b.resources[i])
+
+            # When the process is complete free the process resources
+            # into the system pool
+            for i in range(self.b.num_res):
+                self.b.resources[i] += self.b.allocation[process][i]
+
+
+if __name__ == "__main__":
+    unittest.main()
