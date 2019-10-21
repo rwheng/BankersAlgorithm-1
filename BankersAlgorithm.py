603e4b78c65577b81c1f1bb881c1599743acc960
adday <adday@email.arizona.edu>
2019-10-21 09:29:29 -0400

    Added safety algorithm

--- BankersAlgorithm.py
+++ BankersAlgorithm.py
@@ -1,17 +1,30 @@
 from typing import List
-
+from typing import Tuple
 
 
 class BankersAlgorithm:
     def __init__(self, num_proc: int, num_rec: int, resources: List[int],
                  allocation: List[List[int]], maximum: List[List[int]]):
+        """Initialize the Bankers Algorithm
+
+        Args:
+            num_proc (int): Number of processes
+            num_rec (int): Number of resources
+            resources (List[int]): Quantity of resources available
+            allocation (List[List[int]]): Current resource allocation
+                                          for processes
+            maximum (List[List[int]]): Maximum possible resource allocation
+        """
+
+        # Update all of the datamembers
         self.num_proc = num_proc
         self.num_rec = num_rec
         self.resources = resources
         self.allocation = allocation
         self.maximum = maximum
-        self.safety()
 
+        # Check the safety of the current configuration
+        self.safety()
 
     def calculate_available(self):
         available = [0] * self.num_rec
@@ -25,27 +38,58 @@ class BankersAlgorithm:
         return [[self.maximum[i][j] - self.allocation[i][j]
                  for j in range(self.num_rec)] for i in range(self.num_proc)]
 
-    def safety(self):
+    def request(self, proc_num: int, resource_req: List[int]) -> bool:
+
+    def safety(self) -> Tuple[bool, List[int]]:
+        """Check the safety of the current state of the system.
+        
+        Returns:
+            Tuple[bool, List[int]]: 2-tuple containing if the system is safe,
+                                    and if so the safe process order
+        """                    
+        # Start all processes out as unfinished
         finish = [False] * self.num_proc
+
+        # Calculate the current avaliable resources
         work = self.calculate_available()
+
+        # Caclulate the number of resources each process could request
         need = self.calculate_need()
+
+        # Set the flag for the loop
         done = False
 
-        # Loop while all of finish is not true
+        # Create a queue to store the process order
+        process_order = []
+
+        # Loop while the done flag is unset
         while(not done):
-            # Find an index i such that both finish[i] == false and need[i] <= work
+            # Find an index i such that both finish[i] == false and
+            # need[i] <= work[i] for all i in need and work
+
             found_i = False
             for i in range(self.num_proc):
-                if not finish[i] and need[i] <= work[i]:
-                    work[i] = [self.allocation[i][j] + work[i][j] for j in len(self.allocation[i])]
-                    found_i = True
+                if finish[i]:
+                    continue
 
-            if not found_i: 
+                can_do = True
+                for j in range(self.num_rec):
+                    if need[i][j] > work[j]:
+                        can_do = False
+
+                if can_do:
+                    print("Finishing: {}".format(i))
+                    found_i = True
+                    process_order.append(i)
+                    finish[i] = True
+                    for j in range(self.num_rec):
+                        work[j] += need[i][j]
+                    break
+
+            if not found_i:
                 done = True
 
-        print(finish)
-
-
+        return min(finish), process_order
 
 
 
